fafca68fcc260c1c167f76aabf528ee975517d45
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2022-11-09 13:29:18 +0200

    asd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,5 @@
-// Add imports above this line
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-// Change code below this line
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 
 const galleryDiv = document.querySelector('.gallery');
