7de6e621f51af771f128b1757b65b70106ab37fd
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2022-11-09 14:40:58 +0200

    1.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,24 @@
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 
 const galleryDiv = document.querySelector('.gallery');
 
-const galleryMarkup = makeGalleryMarkup(galleryItems);
-
-galleryDiv.insertAdjacentHTML('beforeend', galleryMarkup);
-
 // galleryDiv.addEventListener('click', onGalleryClick);
 
-function makeGalleryMarkup(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-      <a class="gallery__link href="${original}">
-    <img class="gallery__image" src="${preview}"
-    data-source="${original}"
-    alt="${description}"
-    /></a></div>`;
-    })
-    .join('');
-}
+const makeGalleryMarkup = galleryItems
+  .map(
+    item =>
+      `<a class="gallery__item href="${item.original}">
+    <img class="gallery__image" src="${item.preview}"
+    alt="${item.description}"
+    title="${item.description}"
+    /></a>`
+  )
+  .join('');
+
+galleryDiv.insertAdjacentHTML('beforeend', makeGalleryMarkup);
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
-  captionsData: 'alt',
 });
