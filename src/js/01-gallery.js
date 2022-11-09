8e47938d931dea20563606e032dcef3d1f2bf209
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2022-11-09 13:14:25 +0200

    save

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+import { galleryItems } from './gallery-items';
 
-console.log(galleryItems);
+const galleryDiv = document.querySelector('.gallery');
+
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+
+galleryDiv.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// galleryDiv.addEventListener('click', onGalleryClick);
+
+function makeGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link href="${original}">
+    <img class="gallery__image" src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    /></a></div>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
