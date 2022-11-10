ddb92879d3e2d4f5ce70a0eca0136c7710ec5c2b
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2022-11-10 11:16:19 +0200

    2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+var throttle = require('lodash.throttle');
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const checkpoint = 'vimeo-videoplayer-current-time';
+const currentTime = localStorage.getItem(checkpoint)
+  ? localStorage.getItem(checkpoint)
+  : 0;
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(currentTime);
+
+function getCurrentTime(e) {
+  localStorage.setItem(checkpoint, e.seconds);
+}
