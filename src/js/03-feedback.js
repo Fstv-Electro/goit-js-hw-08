44c28f29a277cbd4ba6f441c2323c9b10e1f1363
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2022-11-10 15:27:23 +0200

    bug fix task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,37 +2,67 @@ import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
 const localStorageKey = 'feedback-from-state';
+const formData = {};
+
+formEl.addEventListener('input', throttle(onInput, 500));
+formEl.addEventListener('submit', onSubmit);
 
 storageForm();
 
-function onInput() {
-  const formData = new FormData(formEl);
-  let userForm = {};
-  formData.forEach((value, name) => (userForm[name] = value));
-  localStorage.setItem(localStorageKey, JSON.stringify(userForm));
+function onInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
 }
 
-formEl.addEventListener('input', throttle(onInput, 500));
+function onSubmit(e) {
+  e.preventDefault();
+  formData.email = formEl.elements.email.value;
+  formData.message = formEl.elements.message.value;
+
+  console.log(formData);
+
+  formEl.reset();
+  localStorage.removeItem(localStorageKey, JSON.stringify(formData));
+}
 
 function storageForm() {
-  let parseForm = localStorage.getItem('localStorageKey');
-  if (parseForm) {
-    parseForm = JSON.parse(parseForm);
-    console.log(parseForm);
-    Object.entries(parseForm).forEach(([name, value]) => {
-      formEl.elements[name].value = value;
-    });
+  const saveForm = localStorage.getItem(localStorageKey);
+  if (saveForm) {
+    const parseForm = JSON.parse(saveForm);
+    for (const parse in parseForm) {
+      if (parseForm.hasOwnProperty(parse)) {
+        formEl.elements[parse].value = parseForm[parse];
+        formData[parse] = parseForm[parse];
+      }
+    }
   }
 }
 
-formEl.addEventListener('submit', onSubmit);
+// function storageForm() {
+//   let parseForm = localStorage.getItem('localStorageKey');
+//   if (parseForm) {
+//     parseForm = JSON.parse(parseForm);
+//     console.log(parseForm);
+//     Object.entries(parseForm).forEach(([name, value]) => {
+//       formEl.elements[name].value = value;
+//       formData.elements[name].value = value;
+//     });
+//   }
+// }
 
-function onSubmit(evt) {
-  evt.preventDefault();
-  localStorage.removeItem(localStorageKey);
-  let userForm = {};
-  const formData = new FormData(formEl);
-  formData.forEach((value, name) => (userForm[name] = value));
-  console.log(userForm);
-  formEl.reset();
-}
+// function onInput() {
+//   const formData = new FormData(formEl);
+//   let userForm = {};
+//   formData.forEach((value, name) => (userForm[name] = value));
+//   localStorage.setItem(localStorageKey, JSON.stringify(userForm));
+// }
+
+// function onSubmit(evt) {
+//   evt.preventDefault();
+//   localStorage.removeItem(localStorageKey);
+//   let userForm = {};
+//   // const formData = new FormData(formEl);
+//   formData.forEach((value, name) => (userForm[name] = value));
+//   console.log(userForm);
+//   formEl.reset();
+// }
